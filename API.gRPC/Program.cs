46afef444f267cc86850635da6bb1180efe7f16b
nbn <nick@knnlabs.com>
2024-05-22 22:03:00 -0700

    gRPC Service: Added load/save operation; save on shutdown event

--- API.gRPC/Program.cs
+++ API.gRPC/Program.cs
@@ -1,11 +1,35 @@
+using Microsoft.Extensions.Hosting;
+using Neighborly;
 using Neighborly.API;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddSingleton<VectorDatabase>();
 
 var app = builder.Build();
+
+// Get the application lifetime object
+var lifetime = app.Services.GetRequiredService<IHostApplicationLifetime>();
+
+// Get the VectorDatabase instance
+var vectorDatabase = app.Services.GetRequiredService<VectorDatabase>();
+
+var databasePath = Environment.GetEnvironmentVariable("DATABASE_PATH");
+if (string.IsNullOrEmpty(databasePath))
+{
+    databasePath = "";
+}
+
+// Load Database on application start
+vectorDatabase.Load(databasePath);
+
+// Save Database on application shutdown
+lifetime.ApplicationStopping.Register(() =>
+{
+    vectorDatabase.Save(databasePath);
+});
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<VectorService>();
