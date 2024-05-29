c9d655767bbb5f0a0ac1b9c7cf42cbe96e1b8498
nbn <nick@knnlabs.com>
2024-05-29 14:00:49 -0700

    Added comments to getter/setters and functions for Vector and VectorDatabase classes

--- Neighborly/VectorDatabase.cs
+++ Neighborly/VectorDatabase.cs
@@ -3,6 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace Neighborly;
+
+/// <summary>
+/// Represents a database for storing and searching vectors.
+/// </summary>
 public class VectorDatabase : ICollection<Vector>
 {
     private DiskBackedList<Vector> _vectors = new DiskBackedList<Vector>();
@@ -10,21 +14,40 @@ public class VectorDatabase : ICollection<Vector>
     private ISearchMethod _searchStrategy = new LinearSearch();
     private ReaderWriterLockSlim _rwLock = new ReaderWriterLockSlim();
     private StorageOptionEnum _storageOption = StorageOptionEnum.Auto;
+
+    /// <summary>
+    /// Gets the number of vectors in the database.
+    /// </summary>
     public int Count => _vectors.Count;
 
+    /// <summary>
+    /// Gets a value indicating whether the database is read-only.
+    /// </summary>
     public bool IsReadOnly => false;
 
+    /// <summary>
+    /// Gets the storage option used by the database.
+    /// </summary>
     public StorageOptionEnum StorageOption
     {
         // TODO: Implement the ability to move the storage from memory to disk while in operation
         get { return _storageOption; } 
     }
+
+    /// <summary>
+    /// Gets or sets the search method used by the database.
+    /// This can be changed at runtime to switch between search methods.
+    /// </summary>
     public ISearchMethod SearchMethod
     {
         get { return _searchStrategy; }
         set { _searchStrategy = value; }
     }
 
+    /// <summary>
+    /// Adds a range of vectors to the database.
+    /// </summary>
+    /// <param name="items">The collection of vectors to add.</param>
     public void AddRange(IEnumerable<Vector> items)
     {
         _rwLock.EnterWriteLock();
@@ -35,6 +58,10 @@ public class VectorDatabase : ICollection<Vector>
         finally { _rwLock.ExitWriteLock(); }
     }
 
+    /// <summary>
+    /// Removes a range of vectors from the database.
+    /// </summary>
+    /// <param name="items">The collection of vectors to remove.</param>
     public void RemoveRange(IEnumerable<Vector> items)
     {
         _rwLock.EnterWriteLock();
@@ -51,6 +78,12 @@ public class VectorDatabase : ICollection<Vector>
         }
     }
 
+    /// <summary>
+    /// Updates an existing vector in the database.
+    /// </summary>
+    /// <param name="oldItem">The vector to be updated.</param>
+    /// <param name="newItem">The updated vector.</param>
+    /// <returns>True if the vector was successfully updated; otherwise, false.</returns>
     public bool Update(Vector oldItem, Vector newItem)
     {
         _rwLock.EnterWriteLock();
@@ -76,6 +109,10 @@ public class VectorDatabase : ICollection<Vector>
         return false;
     }
 
+    /// <summary>
+    /// Adds a vector to the database.
+    /// </summary>
+    /// <param name="item">The vector to add.</param>
     public void Add(Vector item)
     {
         _rwLock.EnterWriteLock();
@@ -92,6 +129,9 @@ public class VectorDatabase : ICollection<Vector>
         finally { _rwLock.ExitWriteLock(); }
     }
 
+    /// <summary>
+    /// Removes all vectors from the database.
+    /// </summary>
     public void Clear()
     {
         _rwLock.EnterWriteLock();
@@ -102,21 +142,40 @@ public class VectorDatabase : ICollection<Vector>
         finally { _rwLock.ExitWriteLock(); }
     }
 
+    /// <summary>
+    /// Determines whether the database contains a specific vector.
+    /// </summary>
+    /// <param name="item">The vector to locate in the database.</param>
+    /// <returns>True if the vector is found in the database; otherwise, false.</returns>
     public bool Contains(Vector item)
     {
         return _vectors.Contains(item);
     }
 
+    /// <summary>
+    /// Copies the vectors of the database to an array, starting at a particular array index.
+    /// </summary>
+    /// <param name="array">The one-dimensional array that is the destination of the elements copied from the database.</param>
+    /// <param name="arrayIndex">The zero-based index in array at which copying begins.</param>
     public void CopyTo(Vector[] array, int arrayIndex)
     {
         _vectors.CopyTo(array, arrayIndex);
     }
 
+    /// <summary>
+    /// Returns an enumerator that iterates through the vectors in the database.
+    /// </summary>
+    /// <returns>An enumerator for the vectors in the database.</returns>
     public IEnumerator<Vector> GetEnumerator()
     {
         return _vectors.GetEnumerator();
     }
 
+    /// <summary>
+    /// Removes a specific vector from the database.
+    /// </summary>
+    /// <param name="item">The vector to remove.</param>
+    /// <returns>True if the vector was successfully removed; otherwise, false.</returns>
     public bool Remove(Vector item)
     {
         _rwLock.EnterWriteLock();
@@ -138,11 +197,21 @@ public class VectorDatabase : ICollection<Vector>
         return result;
     }
 
+    /// <summary>
+    /// Returns an enumerator that iterates through the vectors in the database.
+    /// </summary>
+    /// <returns>An enumerator for the vectors in the database.</returns>
     IEnumerator IEnumerable.GetEnumerator()
     {
         return GetEnumerator();
     }
 
+    /// <summary>
+    /// Searches for the k nearest neighbors to a given query vector.
+    /// </summary>
+    /// <param name="query">The query vector.</param>
+    /// <param name="k">The number of nearest neighbors to retrieve.</param>
+    /// <returns>A list of the k nearest neighbors to the query vector.</returns>
     public IList<Vector> Search(Vector query, int k)
     {
         try
@@ -159,6 +228,11 @@ public class VectorDatabase : ICollection<Vector>
         }
     }
 
+    /// <summary>
+    /// Loads vectors from a specified file path.
+    /// </summary>
+    /// <param name="path">The file path to load the vectors from.</param>
+    /// <param name="createOnNew">Indicates whether to create a new file if it doesn't exist.</param>
     public void Load(string path, bool createOnNew = true)
     {
 #if Save_VectorFiles
@@ -215,16 +289,31 @@ public class VectorDatabase : ICollection<Vector>
 
     }
 
+    /// <summary>
+    /// Retrieves all vectors that match a specified predicate.
+    /// </summary>
+    /// <param name="match">The predicate to match against.</param>
+    /// <returns>A list of vectors that match the specified predicate.</returns>
     public List<Vector> FindAll(Predicate<Vector> match)
     {
         return _vectors.FindAll(match);
     }
 
+    /// <summary>
+    /// Retrieves the first vector that matches a specified predicate.
+    /// </summary>
+    /// <param name="match">The predicate to match against.</param>
+    /// <returns>The first vector that matches the specified predicate.</returns
     public Vector Find(Predicate<Vector> match)
     {
         return _vectors.Find(match);
     }
 
+    /// <summary>
+    /// Saves the vectors to a specified file path.
+    /// (In the gRPC server, this method will be called when the host OS sends a shutdown signal.)
+    /// </summary>
+    /// <param name="path">The file path to save the vectors to.</param>
     public void Save(string path)
     {
         if (!Directory.Exists(path))
