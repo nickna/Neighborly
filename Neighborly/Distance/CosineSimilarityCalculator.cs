b03db55b19d049f7e705235cc839db0ae703a0c6
Nick Nassiri <nick@knnlabs.com>
2025-06-26 17:28:59 -0700

    perf: Optimize Distance Calculations Using Unsafe Code #148

--- Neighborly/Distance/CosineSimilarityCalculator.cs
+++ Neighborly/Distance/CosineSimilarityCalculator.cs
@@ -1,24 +1,35 @@
-﻿namespace Neighborly.Distance;
+namespace Neighborly.Distance;
 
 /// <summary>
 /// Calculates distance metric using Cosine similarity
 /// </summary>
 public sealed class CosineSimilarityCalculator : AbstractBatchDistanceCalculator
 {
-    protected override float CalculateDistanceCore(Vector vector1, Vector vector2)
+    protected override unsafe float CalculateDistanceCore(Vector vector1, Vector vector2)
     {
         float dotProduct = 0;
         float magnitudeA = 0;
         float magnitudeB = 0;
-        for (int i = 0; i < vector1.Dimension; i++)
+        var dimension = vector1.Dimension;
+
+        fixed (float* p1 = vector1.Values, p2 = vector2.Values)
         {
-            dotProduct += vector1.Values[i] * vector2.Values[i];
-            magnitudeA += vector1.Values[i] * vector1.Values[i];
-            magnitudeB += vector2.Values[i] * vector2.Values[i];
+            for (var i = 0; i < dimension; i++)
+            {
+                dotProduct += p1[i] * p2[i];
+                magnitudeA += p1[i] * p1[i];
+                magnitudeB += p2[i] * p2[i];
+            }
         }
 
         magnitudeA = MathF.Sqrt(magnitudeA);
         magnitudeB = MathF.Sqrt(magnitudeB);
+
+        if (magnitudeA == 0 || magnitudeB == 0)
+        {
+            return 0;
+        }
+
         return dotProduct / (magnitudeA * magnitudeB);
     }
 }
--- Neighborly/Distance/EuclideanDistanceCalculator.cs
+++ Neighborly/Distance/EuclideanDistanceCalculator.cs
@@ -1,4 +1,4 @@
-﻿namespace Neighborly.Distance;
+namespace Neighborly.Distance;
 
 /// <summary>
 /// Calculates distance using Euclidean math
@@ -12,13 +12,18 @@ public sealed class EuclideanDistanceCalculator : AbstractBatchDistanceCalculato
     /// </summary>
     internal static EuclideanDistanceCalculator Instance { get; } = new EuclideanDistanceCalculator();
 
-    protected override float CalculateDistanceCore(Vector vector1, Vector vector2)
+    protected override unsafe float CalculateDistanceCore(Vector vector1, Vector vector2)
     {
         float sum = 0;
-        for (int i = 0; i < vector1.Dimension; i++)
+        var dimension = vector1.Dimension;
+
+        fixed (float* p1 = vector1.Values, p2 = vector2.Values)
         {
-            float diff = vector1.Values[i] - vector2.Values[i];
-            sum += diff * diff;
+            for (var i = 0; i < dimension; i++)
+            {
+                var diff = p1[i] - p2[i];
+                sum += diff * diff;
+            }
         }
 
         return MathF.Sqrt(sum);
